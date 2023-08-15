72aabf3b0930c2bae9eacce030d9019f2b3413aa
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-08-15 13:04:32 +0500

    Updated

---
+++ logic.js
@@ -0,0 +1,15 @@
+const hrs = document.getElementById('hrs');
+const min = document.getElementById('min');
+const sec = document.getElementById('sec');
+
+
+
+
+setInterval(()=>{
+
+
+    let currenttime = new Date();
+    hrs.innerText =  currenttime.getHours();
+    min.innerText = currenttime.getMinutes();
+    sec.innerText = currenttime.getSeconds();
+},1000);
